e8e775ce2580f04d791d39e47d9fefbeab890e71
erik-andreasson-nti-johanneberg <Erik.andreasson@elev.ga.ntig.se>
2022-11-11 11:42:53 +0100

    Task 2 completed

--- Car.java
+++ Car.java
@@ -1,17 +1,34 @@
 import java.awt.*;
 
-public class Car {
+public class Car implements Movable{
     private int nrDoors;
     private double enginePower;
     private double currentSpeed;
     private Color color;
+    private double xCord;
+    private double yCord;
+    private Direction direction;
+    private int xChange;
+    private int yChange;
     //. private String modelName;
+
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
 
     public Car(int nrDoors, double enginePower, Color color){
         this.nrDoors = nrDoors;
         this.color = color;
         this.enginePower = enginePower;
         this.currentSpeed = 0;
+        this.xCord = 0;
+        this.yCord = 0;
+        this.direction = Direction.UP;
+        this.xChange = 0;
+        this.yChange = -1;
     }
 
     public int getNrDoors(){
@@ -61,6 +78,58 @@ public class Car {
     // TODO fix this method according to lab pm
     public void brake(double amount){
         decrementSpeed(amount);
+    }
+
+    @Override
+    public void move() {
+        this.xCord += this.xChange * currentSpeed;
+        this.yCord += this.yChange * currentSpeed;
+    }
+
+    @Override
+    public void turnLeft() {
+        switch (this.direction) {
+            case UP:
+                this.xChange = -1;
+                this.yChange = 0;
+                this.direction = Direction.LEFT;
+            case DOWN:
+                this.xChange = 1;
+                this.yChange = 0;
+                this.direction = Direction.RIGHT;
+            case LEFT:
+                this.xChange = 0;
+                this.yChange = 1;
+                this.direction = Direction.DOWN;
+            case RIGHT:
+                this.xChange = 0;
+                this.yChange = -1;
+                this.direction = Direction.UP;
+        }
+
+        
+    }
+
+    @Override
+    public void turnRight() {
+        switch (this.direction) {
+            case UP:
+                this.xChange = 11;
+                this.yChange = 0;
+                this.direction = Direction.RIGHT;
+            case DOWN:
+                this.xChange = -1;
+                this.yChange = 0;
+                this.direction = Direction.LEFT;
+            case LEFT:
+                this.xChange = 0;
+                this.yChange = -1;
+                this.direction = Direction.UP;
+            case RIGHT:
+                this.xChange = 0;
+                this.yChange = 1;
+                this.direction = Direction.DOWN;
+        }
     }
 }
 
--- Saab95.java
+++ Saab95.java
@@ -2,26 +2,21 @@ import java.awt.*;
 
 public class Saab95 extends Car{
 
-    public boolean turboOn;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // The Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // The color of the car
-    public String modelName; // The car model name
+    private boolean turboOn;
+    private String modelName; // The car model name
     
     public Saab95(){
         super(2, 125.0, Color.red);
         this.modelName = "Saab95";
         this.turboOn = false;
-        this.currentSpeed = 0;
     }
 
     public void setTurboOn(){
-	    turboOn = true;
+	    Boolean turboOn = true;
     }
 
     public void setTurboOff(){
-	    turboOn = false;
+	    Boolean turboOn = false;
     }
 
     @Override
